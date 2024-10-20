ae468bcc31a60094f19f0d89986dd37699087c36
NitheeshB-33 <nitheeshb359@gmail.com>
2024-10-20 14:48:09 +0530

    final

---
+++ script.js
@@ -0,0 +1,10 @@
+// account popup 
+//submit button (contact section) clicks popup 
+
+function showAlert() {
+    alert("Message Successfully Sends !")
+}
+
+function show() {
+    alert("THIS FEATURE CURRENTLY DISABLED! AVAILABLE SOON !")
+}
